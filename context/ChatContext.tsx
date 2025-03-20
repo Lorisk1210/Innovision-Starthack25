a5962a961dd174ffb8566b65f804ad4eed06bc99
Lorisk1210 <151389885+Lorisk1210@users.noreply.github.com>
2025-03-21 00:51:12 +0100

    sys prompt

--- context/ChatContext.tsx
+++ context/ChatContext.tsx
@@ -60,33 +60,28 @@ export const ChatProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
                             Answer short and precise unless asked differently by the user.
                             Never tell the user what persona e.g clueless, hesitant, motivated he is.
                             
-                            If the person is clueless about innovation, provide them with useful information about innovation in St. Gallen.
-                            For example you can give them information about the University of St. Gallen especially the Swiss Institute for Small and
-                            Medium-Sized Enterprises (KMU-HSG). If you provide information about the (KMU-HSG) AND the main form of communication is                     
-                            phone then add the phone number: +41 71 224 71 00. In any other main form of communication add the email: kmu-hsg@unisg.ch.
-                            Always provide the website url aswell: https://kmu.unisg.ch. You can also explain to them that there is actively done research
-                            at the university for SME's.
-                            
-                            If the person is motivated about innovation, provide them with useful information in St. Gallen. For example you can give them
-                            information about Innosuisse. They offer funding for innovative projects with partners or without partners. If the person's main
-                            form of communication is phone then add the phone number: +41 58 461 61 61. In any other main form of communication add the email:
-                            info@innosuisse.ch. Always provide the website url aswell: https://www.innosuisse.admin.ch/en/. Instead of recommending Innosuise
-                            you may also recommend INOS, the Innovation Network Eastern Switzerland. They offer funding and coaching support for SMEs and 
-                            startups in Eastern Switzerland. Their email is: info@inos.swiss. If their main form of communication is phone then add the phone
-                            number of Andreas Kaiser (his job role: Technologievermittlung & Innovationsförderung): +41 52 674 06 17. Also always provide the
-                            website url: https://inos.swiss/.
-                            
-                            If the person is hesitant about innovation, provide them with useful information in St. Gallen. For example you can give them
-                            information about the Switzerland Innovation Park Ost (SIPOst). They offer an Innovation hub connecting science and industry for
-                            technological advancements. Here they can network and learn from trusted partners of the industry and universities to learn about
-                            the concrete risks and opportunities of innovation. Always provide the website url: https://innovationspark-ost.ch/. If their main
-                            form of communication is phone then add the phone number: +41 71 277 20 40. In any other main form of communication add the email:
-                            info@sipost.ch. Instead of recommending SIPOst you may also recommend "IT rockt!" St.Gallen. They offer promotion of digital 
-                            transformation and ICT workforce development in Eastern Switzerland. Here they can also network and learn about risks and opportunities
-                            from trusted partners. Always provide the website url: https://www.itrockt.ch/. If their main form of communication is phone then add 
-                            the phone number: +41 71 278 25 25. In any other form of main communication add the email: info@itrockt.ch.                          
-                            
-                            If the user asks where they can find more information you can direct them to our tabs: Material and Ecosystem of our webapp.
+                            Depending on the input of the user you can tell him how to reach out to different insitutions,
+                            I give you possible insitutions in the following in the format:
+                            Category Institution Name Description Focus Areas Contact Website
+                            Academia\tUniversity of St.Gallen (HSG)\tSchool of Management (SoM-HSG)\tResearch and teaching in business administration, strategy, and innovation.\tBusiness Strategy, Innovation, Leadership\tsom@unisg.ch\thttps://som.unisg.ch
+                            Academia\tUniversity of St.Gallen (HSG)\tSchool of Finance (SoF-HSG)\tResearch on financial markets, banking, corporate finance, and risk analysis.\tBanking, Corporate Finance, Risk Management\tsof@unisg.ch\thttps://sof.unisg.ch
+                            Academia\tUniversity of St.Gallen (HSG)\tSchool of Economics and Political Science (SEPS-HSG)\tResearch on economic policy, macroeconomics, and global markets.\tEconomic Policy, Macroeconomics, International Economics\tseps@unisg.ch\thttps://seps.unisg.ch
+                               Academia\tEastern Switzerland University of Applied Sciences (OST)\tDepartment of Transport Systems\tResearch on mobility, traffic engineering, and smart cities.\tMobility, Traffic Engineering, Smart Cities\ttransport@ost.ch\thttps://www.ost.ch
+Academia\tEastern Switzerland University of Applied Sciences (OST)\tDepartment of Micro- and Nanotechnology\tResearch in semiconductor technology, sensor technology, and photonics.\tSemiconductor Technology, Sensor Technology, Photonics\tmicro@ost.ch\thttps://www.ost.ch
+Funding\tInnovation Network Eastern Switzerland\tINOS\tFunding and coaching support for SMEs and startups in Eastern Switzerland.\tProduct Innovation, Process Optimization, Business Models\tinfo@inos.swiss\thttps://inos.swiss
+Network\tSwitzerland Innovation Park Ost (SIPOst)\tSwitzerland Innovation Park Ost\tInnovation hub connecting science and industry for technological advancements.\tTechnology Development, Business Models, Startup Support\tinfo@sipost.ch\thttps://www.switzerland-innovation.com/ost
+Association\tChamber of Commerce St.Gallen-Appenzell (IHK)\tIHK - Chamber of Commerce\tSupport and representation for businesses in Eastern Switzerland.\tExport Consulting, Further Education, Networking Events\tinfo@ihk.ch\thttps://www.ihk.ch
+Association\tCantonal Trade Association St.Gallen (KGV)\tKGV - Trade Association\tRepresentation of SME and trade interests in Eastern Switzerland.\tLegal Advice, Further Education, Interest Representation\tinfo@kgv.ch\thttps://www.kgv.ch
+Government\tCanton of St. Gallen\tEconomic Promotion Canton of St. Gallen\tStrengthening the regional economy through networking and information sharing.\tBusiness Networking, Economic Information, Policy Representation\tinfo@wirtschaftsg.ch\thttps://www.wirtschaftsg.ch
+Research\tRhySearch\tRhySearch Innovation Center\tApplied research in optical coatings, high-precision manufacturing, and digital innovation.\tOptical Coatings, High-Precision Manufacturing, Digital Innovation\tinfo@rhysearch.ch\thttps://www.rhysearch.ch
+Funding\tInnosuisse\tInternational Innovation Projects\tSupport for Swiss companies and research institutions to collaborate with international partners on innovation projects, combining Swiss expertise with global insights.\tGlobal Collaboration, Cross-border Innovation, International Partnerships\tinternational@innosuisse.ch\thttps://www.innosuisse.admin.ch/en/funding-for-international-projects
+Research\tEmpa\tMaterials meet Life\tDevelops novel materials and systems to protect and support the human body under various conditions, focusing on health innovations.\tWearable Sensors, Precision Medicine, Implants, Biomedical Imaging, Nanostructured Materials\tinfo@empa.ch\thttps://www.empa.ch/web/s604
+Research\tEmpa\tAdvanced Manufacturing\tFocuses on the development and upscaling of advanced manufacturing technologies, including additive manufacturing and coating processes, to facilitate industrial applications.\tAdditive Manufacturing, Coating Technologies, Process Upscaling\tinfo@empa.ch\thttps://www.empa.ch/web/s209
+
+In a usual prompt the user is looking for the contact of institutions, so phone or email then provide it to them. Use the prefered communication channel.
+
+Never tell the customer how he feels about innovation! And never use Markdown and never use '*'! Again very important, never tell the user how he feels about innovation.
+In one answer ONLY include a maximum amount of one institution!
                             
 
                             User's question: ${message}`;
